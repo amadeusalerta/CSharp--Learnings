5c6d75887c266ad54fda36326d98cba849050736
Amadeus Alerta <efe3365@gmail.com>
2022-12-03 11:27:18 +0300

    Baştan C#'a hızlı bir tekrar
    Baştan C#'a hızlı bir tekrar ve temelleri yazma

---
+++ Episode 1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MyProgram
+{
+
+
+    class AgeCalculator
+    {
+        static void Main(string[] args)
+        {
+
+            int age = 18;
+            double height = 172;
+            bool alive = true;
+            char symbol = '@';
+            String name = "Amadeus";
+            const int passwd = 1928374650;
+
+            Console.WriteLine("Your name is " + name);
+            Console.WriteLine("Your age is " + age);
+            Console.WriteLine("Your height is " + height);
+            Console.WriteLine("Are you alive " + alive);
+            Console.WriteLine("Your symbol is " + symbol);
+            Console.WriteLine("Your Password is " + passwd);
+;
+            Console.ReadKey();
+        }
+
+    }
+
+
+
+}
